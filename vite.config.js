142d49e7e8d237ce9f6ce9b4047e8a721ef6f327
Cibulya <cibulyadev@gmail.com>
2025-06-27 02:02:24 +0200

    refactor vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/vue-course/',
+  base:'./',
   plugins: [
     vue(),
     vueDevTools(),
