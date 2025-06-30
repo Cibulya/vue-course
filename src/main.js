0c8c0eccf9e65412bc1b610fa14562f8254b75cc
Cibulya <cibulyadev@gmail.com>
2025-06-30 14:42:59 +0200

    setup files for lesson

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './theme.css';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './theme.css'
 
-
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).mount('#app')
