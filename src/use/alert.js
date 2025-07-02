c5b1b9b323a63e4ce2cfac3fe05e1e537b06d6ae
Cibulya <cibulyadev@gmail.com>
2025-07-02 15:26:49 +0200

    10-lesson setup cleean project with ts

--- src/use/alert.js
+++
@@ -1,9 +0,0 @@
-import { ref } from "vue"
-
-export const useAlert = () => {
-    const alert = ref(false);
-    const toggle = () => alert.value = !alert.value;
-    const closeAlert = () => alert.value = false;
-
-    return { alert,toggle,closeAlert}
-}
